406f8c687c3a36bac9d77936d812c84be0051a61
rhw <ruohanw89@gmail.com>
2020-10-19 00:56:16 -0700

    connection to ApolloClient

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// add these two library import statements
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+// connect to backend server's
+const client = new ApolloClient({
+  uri: 'http://localhost:3005/graphql',
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
