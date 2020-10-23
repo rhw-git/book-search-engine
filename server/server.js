e20524edd356c87361bca0741a9ac7ba1bd8d4ac
rhw <ruohanw89@gmail.com>
2020-10-23 04:36:45 -0700

    updated port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const db = require('./config/connection');
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 // import the middleware function
 const { authMiddleware } = require('./utils/auth');
