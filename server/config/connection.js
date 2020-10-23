bfa4c51791d56049fa63466b05a238782e3bed05
rhw <ruohanw89@gmail.com>
2020-10-23 04:01:02 -0700

    add a catch error at mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
