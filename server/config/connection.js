60f6677cc6b0530501dc20158f54458b0a0debc1
rhw <ruohanw89@gmail.com>
2020-10-23 05:28:45 -0700

    connection update

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-  })
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+// .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
